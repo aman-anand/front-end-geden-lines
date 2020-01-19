1cb1b0a9e5da16950a4cf80fade6b2a9eeec0353
Shubham Anand <shubham.anand@leadschool.in>
2020-01-19 23:34:46 +0530

    build created

---
+++ build/precache-manifest.4618e05a13593f5b0e2c034bd54948dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1afd6fc4e5f61ee729e00904e255c53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c522362c10564cca4c6a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b4b7240c2a22f8d1773c",
+    "url": "/static/js/2.c45a0012.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c45a0012.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c522362c10564cca4c6a",
+    "url": "/static/js/main.97e90f50.chunk.js"
+  },
+  {
+    "revision": "b630c0e7a4787e3d3e99",
+    "url": "/static/js/runtime-main.74a26dfb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
